7de89122ea6dd1c7c21c6f30538e89ff92e016ce
psycoder01 <aakashchy13@gmail.com>
2021-03-08 22:52:25 +0545

    transparency,multi monitor setup

--- .config/bspwm/scripts/monitor.sh
+++ .config/bspwm/scripts/monitor.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/sh
 
-countOfConnectedMonitors=$(bspc query --monitors | wc -l)
+countOfConnectedMonitors=$(xrandr | grep " connected " | wc -l)
 
 if [[ $countOfConnectedMonitors == 1 ]]; then
-    echo "$countOfConnectedMonitors"
-    exec bspc monitor -d I II III IV V VI VII VIII IX X
+    bspc monitor -d I II III IV V VI VII VIII IX X
 else
-    echo "$countOfConnectedMonitors"
-    exec bspc monitor eDP1 -d I IV V VI X 
-    exec bspc monitor HDMI1 -d II III VII VIII IX
+    bspc monitor eDP1 -d I IV V VI X 
+    bspc monitor HDMI1 -d II III VII VIII IX
 fi
